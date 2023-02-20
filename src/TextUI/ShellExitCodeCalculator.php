0374c396c5330c35084029d12c72af4cae825977
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-02-20 14:36:08 +0100

    Implement optional failing on deprecations and notices

--- src/TextUI/ShellExitCodeCalculator.php
+++ src/TextUI/ShellExitCodeCalculator.php
@@ -22,7 +22,7 @@ final class ShellExitCodeCalculator
 
     private const EXCEPTION_EXIT = 2;
 
-    public function calculate(bool $failOnEmptyTestSuite, bool $failOnRisky, bool $failOnWarning, bool $failOnIncomplete, bool $failOnSkipped, TestResult $result): int
+    public function calculate(bool $failOnDeprecation, bool $failOnEmptyTestSuite, bool $failOnIncomplete, bool $failOnNotice, bool $failOnRisky, bool $failOnSkipped, bool $failOnWarning, TestResult $result): int
     {
         $returnCode = self::FAILURE_EXIT;
 
@@ -35,11 +35,7 @@ final class ShellExitCodeCalculator
         }
 
         if ($result->wasSuccessfulIgnoringPhpunitWarnings()) {
-            if ($failOnRisky && $result->hasTestConsideredRiskyEvents()) {
-                $returnCode = self::FAILURE_EXIT;
-            }
-
-            if ($failOnWarning && $result->hasWarningEvents()) {
+            if ($failOnDeprecation && ($result->hasTestTriggeredDeprecationEvents() || $result->hasTestTriggeredPhpDeprecationEvents() || $result->hasTestTriggeredPhpunitDeprecationEvents())) {
                 $returnCode = self::FAILURE_EXIT;
             }
 
@@ -47,7 +43,19 @@ final class ShellExitCodeCalculator
                 $returnCode = self::FAILURE_EXIT;
             }
 
+            if ($failOnNotice && ($result->hasTestTriggeredNoticeEvents() || $result->hasTestTriggeredPhpNoticeEvents())) {
+                $returnCode = self::FAILURE_EXIT;
+            }
+
+            if ($failOnRisky && $result->hasTestConsideredRiskyEvents()) {
+                $returnCode = self::FAILURE_EXIT;
+            }
+
             if ($failOnSkipped && $result->hasTestSkippedEvents()) {
+                $returnCode = self::FAILURE_EXIT;
+            }
+
+            if ($failOnWarning && $result->hasWarningEvents()) {
                 $returnCode = self::FAILURE_EXIT;
             }
         }
